3bf72fa7a4f94542e2856580c022d9055fb19269
Michael Antony Samy G <michaelantonysamyg@Michaels-MacBook-Air-10.local>
2019-03-28 09:03:43 +0530

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular6Basic';
+  title = 'Angular 6';
+  imgURL = "../assets/img1.jpg";
+  event = "click";
+  msg = "";
+  showMsg():void{
+    alert("hai");
+  }
+  backChange(type:any):void{
+      if(type==1){
+        this.imgURL = "../assets/img1.jpg";
+      }else{
+        this.imgURL = "../assets/img2.png";
+      }
+  }
+  printValue(msg:string):void{
+    this.msg = msg;
+  }
 }
